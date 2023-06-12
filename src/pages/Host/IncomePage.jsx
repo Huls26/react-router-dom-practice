6efca91f04b4a4f62a26ae495b8df60a2bf8b45f
Jules Moreno <julesmoreno66@gmail.com>
2023-06-12 15:40:24 +0800

    add placeholder in IncomePage.jsx & ReviewPage.jsx

--- src/pages/Host/IncomePage.jsx
+++ src/pages/Host/IncomePage.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate, useLoaderData } from 'react-router-dom';
 import authReq from '@helper/authReq';
+import imageChart from '@assets/images/income-graph.svg';
 
 export async function loader({ request }) {
   const user = await authReq(request);
@@ -21,7 +22,44 @@ export default function IncomePage() {
     }
   }, [pathname, navigate]);
 
+  const transactionsData = [
+    { amount: 720, date: "Jan 3, '23", id: '1' },
+    { amount: 560, date: "Dec 12, '22", id: '2' },
+    { amount: 980, date: "Dec 3, '22", id: '3' },
+  ];
   return (
-    <div> this IncomePage</div>
+    <section className="host-income">
+      <h1>Income</h1>
+      <p>
+        Last
+        {' '}
+        <span>30 days</span>
+      </p>
+      <h2>$2,260</h2>
+      <img
+        className="graph"
+        src={imageChart}
+        alt="Income graph"
+      />
+      <div className="info-header">
+        <h3>Your transactions (3)</h3>
+        <p>
+          Last
+          {' '}
+          <span>30 days</span>
+        </p>
+      </div>
+      <div className="transactions">
+        {transactionsData.map((item) => (
+          <div key={item.id} className="transaction">
+            <h3>
+              $
+              {item.amount}
+            </h3>
+            <p>{item.date}</p>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
--- src/pages/Host/ReviewPage.jsx
+++ src/pages/Host/ReviewPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate, useLoaderData } from 'react-router-dom';
 import authReq from '@helper/authReq';
+import reviewGraphImg from '@assets/images/reviews-graph.svg';
 
 export async function loader({ request }) {
   const pathname = await authReq(request);
@@ -14,7 +15,7 @@ export async function loader({ request }) {
 
 export default function ReviewPage() {
   const navigate = useNavigate();
-  const { data, pathname } = useLoaderData();
+  const { pathname } = useLoaderData();
 
   useEffect(() => {
     if (pathname) {
@@ -25,7 +26,57 @@ export default function ReviewPage() {
     }
   }, [pathname, navigate]);
 
+  const reviewsData = [
+    {
+      rating: 5,
+      name: 'Elliot',
+      date: 'January 3, 2023',
+      text: 'The beach bum is such an awesome van! Such a comfortable trip. We had it for 2 weeks and there was not a single issue. Super clean when we picked it up and the host is very comfortable and understanding. Highly recommend!',
+      id: '1',
+    },
+    {
+      rating: 5,
+      name: 'Sandy',
+      date: 'December 12, 2022',
+      text: 'This is our third time using the Modest Explorer for our travels and we love it! No complaints, absolutely perfect!',
+      id: '2',
+    },
+  ];
+
   return (
-    <div>{data}</div>
+    <section className="host-reviews">
+      <div className="top-text">
+        <h2>Your reviews</h2>
+        <p>
+          Last
+          {' '}
+          <span>30 days</span>
+        </p>
+      </div>
+      <img
+        className="graph"
+        src={reviewGraphImg}
+        alt="Review graph"
+      />
+      <h3>Reviews (2)</h3>
+      {reviewsData.map((review) => (
+        <div key={review.id}>
+          <div className="review">
+            {
+            // maybe add a star for ratings
+          }
+            {/* {[...Array(review.rating)].map((_, i) => (
+              <BsStarFill className="review-star" key={i} />
+            ))} */}
+            <div className="info">
+              <p className="name">{review.name}</p>
+              <p className="date">{review.date}</p>
+            </div>
+            <p>{review.text}</p>
+          </div>
+          <hr />
+        </div>
+      ))}
+    </section>
   );
 }
